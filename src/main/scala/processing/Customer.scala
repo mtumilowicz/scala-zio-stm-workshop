c5945a247696890dbaf01a8f292ce336b6d7807c
mtu <michal.tumilowicz01@gmail.com>
2024-04-05 21:13:46 +0200

    queue processing

--- src/main/scala/processing/Customer.scala
+++ src/main/scala/processing/Customer.scala
@@ -1,17 +1,18 @@
 package processing
 
-import zio.{Clock, UIO, ZIO}
 import zio.stm.{TQueue, ZSTM}
+import zio.{Clock, UIO}
 
 import java.time.Instant
 
 class Customer
+
 case class Event(instant: Instant, customer: Customer)
 
 def process(queue: TQueue[Event]): UIO[Customer] =
   Clock.instant.flatMap { now =>
-    queue.take
-      .flatMap { case Event(instant, customer) =>
-        if (now.isBefore(instant)) ZSTM.retry else ZSTM.succeed(customer)
-      }.commit.forever
+    (for {
+      event <- queue.take
+      result <- if (now.isBefore(event.instant)) ZSTM.retry else ZSTM.succeed(event.customer)
+    } yield result).commit.forever
   }
