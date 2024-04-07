db3715d650cb17e514f1d45beae298bc5cf46400
mtu <michal.tumilowicz01@gmail.com>
2024-04-08 00:21:28 +0200

    Customer fix

--- src/main/scala/processing/Customer.scala
+++ src/main/scala/processing/Customer.scala
@@ -1,7 +1,8 @@
 package processing
 
-import zio.stm.{TQueue, ZSTM}
-import zio.{Clock, UIO}
+import processing.Event.given
+import zio.stm.{TPriorityQueue, ZSTM}
+import zio.{Clock, UIO, ZIO, ZIOAppArgs, ZIOAppDefault, durationInt}
 
 import java.time.Instant
 
@@ -9,10 +10,31 @@ class Customer
 
 case class Event(instant: Instant, customer: Customer)
 
-def process(queue: TQueue[Event]): UIO[Customer] =
+object Event {
+  given Ordering[Event] = Ordering.by(_.instant)
+}
+
+def process(queue: TPriorityQueue[Event]): UIO[Customer] =
   Clock.instant.flatMap { now =>
     (for {
       event <- queue.take
-      result <- if (now.isBefore(event.instant)) ZSTM.retry else ZSTM.succeed(event.customer)
+      result <- if (now.isBefore(event.instant)) {
+        println("retry");
+        ZSTM.retry
+      } else {
+        println("processed");
+        ZSTM.succeed(event.customer)
+      }
     } yield result).commit.forever
   }
+
+object Process extends ZIOAppDefault {
+
+  override def run: ZIO[ZIOAppArgs, Any, Any] = for {
+    queue <- TPriorityQueue.make[Event]().commit
+    f1 <- process(queue).fork
+    _ <- queue.offer(Event(Instant.parse("2030-12-03T10:15:30.00Z"), new Customer)).commit
+    _ <- queue.offer(Event(Instant.parse("2010-12-03T10:15:30.00Z"), new Customer)).commit.delay(5.seconds)
+    _ <- f1.join
+  } yield ()
+}
