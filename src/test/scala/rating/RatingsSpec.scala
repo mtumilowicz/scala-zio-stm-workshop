49793e92118ac01c3d66bd845bf59b8c60f2e3af
mtu <michal.tumilowicz01@gmail.com>
2024-04-12 00:38:30 +0200

    RatingsSpec.scala

--- src/test/scala/rating/RatingsSpec.scala
+++ src/test/scala/rating/RatingsSpec.scala
@@ -1,9 +1,9 @@
 package rating
 
-import rating.{Ratings, Top}
-import zio.{UIO, ZIO}
+import rating.Ratings
 import zio.stm.{TMap, TRef}
-import zio.test.{Gen, Spec, TestEnvironment, ZIOSpecDefault, assertTrue, check, sized}
+import zio.test.{Gen, Spec, TestEnvironment, ZIOSpecDefault, assertCompletes, assertTrue, check}
+import zio.{UIO, ZIO}
 
 object RatingsSpec extends ZIOSpecDefault {
 
@@ -15,9 +15,9 @@ object RatingsSpec extends ZIOSpecDefault {
             topRef <- TRef.make(Top(5)).commit
             all <- TMap.empty[String, Int].commit
             ratings = Ratings(topRef, all)
-            _      <- ratings.add(id, rating)
-            top    <- topRef.get.commit
-            all    <- all.get(id).commit
+            _ <- ratings.add(id, rating)
+            top <- topRef.get.commit
+            all <- all.get(id).commit
           } yield assertTrue(top.values.contains(rating), all.contains(rating))
         }
       },
@@ -28,7 +28,7 @@ object RatingsSpec extends ZIOSpecDefault {
             topRef <- TRef.make(Top(size)).commit
             all <- TMap.empty[String, Int].commit
             ratings = Ratings(topRef, all)
-            _      <- ratings.addAll(list)
+            _ <- ratings.addAll(list)
             allValues <- all.toMap.commit
             topValues <- ratings.top.get.map(_.values).commit
           } yield assertTrue(
@@ -43,12 +43,35 @@ object RatingsSpec extends ZIOSpecDefault {
             topRef <- TRef.make(Top(size)).commit
             all <- TMap.empty[String, Int].commit
             ratings = Ratings(topRef, all)
-            _      <- ratings.addAll(list)
+            _ <- ratings.addAll(list)
             allValues <- all.toMap.commit
             topValues <- ratings.top.get.map(_.values).commit
           } yield assertTrue(
             topValues == allValues.values.toList.sorted(Ordering[Int].reverse).take(size).toSet,
             list.forall { case (id, rating) => allValues(id) == rating })
+        }
+      },
+      test("indexing many ratings should keep top and all sampled at any time") {
+        check(Gen.listOfBounded(20, 5000)(Gen.uuid.map(_.toString) <*> Gen.int)) { list =>
+          val size = 10
+          for {
+            topRef <- TRef.make(Top(size)).commit
+            all <- TMap.empty[String, Int].commit
+            ratings = Ratings(topRef, all)
+            f1 <- ratings.addAll(list).fork
+            f2 <- (for {
+              allValues <- all.toMap.commit
+              topValues <- ratings.top.get.map(_.values).commit
+              _ <- ZIO.fail("should never fail")
+                .unless(
+                  topValues == allValues.values.toList.sorted(Ordering[Int].reverse).take(size).toSet
+                    && list.forall { case (id, rating) => allValues(id) == rating }
+                )
+            } yield ()
+              ).forever.fork
+            _ <- f1.join
+            _ <- f2.interrupt
+          } yield assertCompletes
         }
       },
     )
