137bfaf75c228f3286daa84779499562a3f2b605
mtu <michal.tumilowicz01@gmail.com>
2024-04-12 15:08:46 +0200

    BankSpec cleanup

--- src/test/scala/bank/BankSpec.scala
+++ src/test/scala/bank/BankSpec.scala
@@ -11,26 +11,23 @@ object BankSpec extends ZIOSpecDefault {
 
   override def spec: Spec[TestEnvironment, Any] =
     suite("Bank")(
-      test("balance of two accounts should remain consistent after transfers") {
-        check(Gen.bigDecimal(0.01, 1.0), genAccountId, genAccountId) { (amount, accountId1, accountId2) =>
+      test("sum of balances of two accounts should remain the same after transfers") {
+        check(Gen.listOfN(100)(Gen.bigDecimal(0.01, 10)), genAccountId, genAccountId) { (amounts, accountId1, accountId2) =>
           for {
             bank <- Bank()
-            account1 <- bank.createBankAccount(accountId1, 10000.0)
-            account2 <- bank.createBankAccount(accountId2, 10000.0)
+            initialBalance = 10000.0
+            account1 <- bank.createBankAccount(accountId1, initialBalance)
+            account2 <- bank.createBankAccount(accountId2, initialBalance)
 
-            _ <- ZIO.foreachParDiscard(1 to 1000) {
-              _ =>
-                bank.transfer(accountId1, accountId2, amount)
-                  .option
-            } <&> ZIO.foreachParDiscard(1 to 1000) {
-              _ =>
-                bank.transfer(accountId2, accountId1, amount)
-                  .option
+            _ <- ZIO.foreachParDiscard(amounts) { amount =>
+              bank.transfer(accountId1, accountId2, amount)
+                .option
             }
 
             balance1 <- bank.getAccount(accountId1).map(_.balance)
             balance2 <- bank.getAccount(accountId2).map(_.balance)
-          } yield assertTrue(balance1 + balance2 == 20000.0)
+            delta = amounts.sum
+          } yield assertTrue(balance1 + balance2 == 2 * initialBalance)
         }
       },
       test("result of successful transfers should be reflected in balances") {
