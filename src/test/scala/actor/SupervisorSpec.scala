9d3f95bdcdaff71ebbe2fbe99e94170628e3332d
mtu <michal.tumilowicz01@gmail.com>
2024-04-12 15:09:04 +0200

    formatting

--- src/test/scala/actor/SupervisorSpec.scala
+++ src/test/scala/actor/SupervisorSpec.scala
@@ -1,14 +1,15 @@
 package actor
 
-import zio.{Random, Scope, ZIO}
 import zio.stm.TReentrantLock
 import zio.test.{Gen, Sized, Spec, TestEnvironment, ZIOSpecDefault, assertCompletes, assertTrue, check}
+import zio.{Random, ZIO}
 
 import scala.collection.mutable
 
 object SupervisorSpec extends ZIOSpecDefault {
 
   def genActorImpl: Gen[Sized, ActorImpl] = Gen.const(new ActorImpl)
+
   override def spec: Spec[TestEnvironment, Any] =
     suite("Supervisor")(
       test("transfer all actors from one supervisor to the other") {
@@ -52,11 +53,11 @@ object SupervisorSpec extends ZIOSpecDefault {
             actorsAll1 <- Random.shuffle((actors1 ++ actors2).toList)
             actorsAll2 <- Random.shuffle(actorsAll1)
             _ <- ZIO.foreachParDiscard(actorsAll1) { actor =>
-                supervisor1.transferTo(actor, supervisor2)
-                  .option
+              supervisor1.transferTo(actor, supervisor2)
+                .option
             } <&> ZIO.foreachParDiscard(actorsAll2) { actor =>
-                supervisor2.transferTo(actor, supervisor1)
-                  .option
+              supervisor2.transferTo(actor, supervisor1)
+                .option
             }
           } yield assertTrue(actors1 ++ actors2 == supervisor1.actors ++ supervisor2.actors)
         }
